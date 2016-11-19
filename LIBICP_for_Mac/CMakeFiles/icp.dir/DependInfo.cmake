32b22f4c1c971c7f14091aff550e046bbeee1ae9
Ryo <Ryo@Ryo-no-MacBook-Air.local>
2016-11-20 00:19:42 +0900

    initial commit

---
+++ LIBICP_for_Mac/CMakeFiles/icp.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Ryo/Desktop/libicp (1)/src/demo.cpp" "/Users/Ryo/Desktop/libicp (1)/CMakeFiles/icp.dir/src/demo.cpp.o"
+  "/Users/Ryo/Desktop/libicp (1)/src/icp.cpp" "/Users/Ryo/Desktop/libicp (1)/CMakeFiles/icp.dir/src/icp.cpp.o"
+  "/Users/Ryo/Desktop/libicp (1)/src/icpPointToPlane.cpp" "/Users/Ryo/Desktop/libicp (1)/CMakeFiles/icp.dir/src/icpPointToPlane.cpp.o"
+  "/Users/Ryo/Desktop/libicp (1)/src/icpPointToPoint.cpp" "/Users/Ryo/Desktop/libicp (1)/CMakeFiles/icp.dir/src/icpPointToPoint.cpp.o"
+  "/Users/Ryo/Desktop/libicp (1)/src/kdtree.cpp" "/Users/Ryo/Desktop/libicp (1)/CMakeFiles/icp.dir/src/kdtree.cpp.o"
+  "/Users/Ryo/Desktop/libicp (1)/src/matrix.cpp" "/Users/Ryo/Desktop/libicp (1)/CMakeFiles/icp.dir/src/matrix.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ LIBICP_for_Mac/CMakeFiles/icp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icp.dir/src/demo.cpp.o"
+  "CMakeFiles/icp.dir/src/icp.cpp.o"
+  "CMakeFiles/icp.dir/src/icpPointToPlane.cpp.o"
+  "CMakeFiles/icp.dir/src/icpPointToPoint.cpp.o"
+  "CMakeFiles/icp.dir/src/kdtree.cpp.o"
+  "CMakeFiles/icp.dir/src/matrix.cpp.o"
+  "icp.pdb"
+  "icp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/icp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
